b623b0f988bbfe252c1f254c1a4be5af4009d87b
gustavo <gustavoantonio.pp@outlook.com>
2020-10-23 10:27:57 -0300

    rodape atualizado

--- src/components/Rodape/index.jsx
+++ src/components/Rodape/index.jsx
@@ -3,9 +3,38 @@ import from './index.css'
 
 function Rodape(){
     return(
-        <footer className="rodape">
-            <div className="logo"></div>
-        </footer>
+        <div className="footer">
+            <div className="logo"><img src=""></img></div>
+            <div className="itens">
+                <h3><a></a></h3>
+                <ul>
+                    <li><a></a></li>
+                    <li><a></a></li>
+                    <li><a></a></li>
+                    <li><a></a></li>
+                </ul>
+            </div>
+            <div className="about">
+                <h3></h3>
+                <a></a>
+            </div>
+            <div className="contact">
+                <ul>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            <div className="social">
+                <ul>
+                    <li><a></a></li>
+                    <li><a></a></li>
+                    <li><a></a></li>
+                </ul>
+            </div>
+            <div className="copyright">
+                <p>© Copyright - Informática NA</p>
+            </div>
+        </div>
     );
 }
 
