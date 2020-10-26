1cfe3bf2aa227c927b8602c61c3ef2b3fc307dc8
Jean Rojas <jeanrojas2007@outlook.com>
2020-10-26 16:05:49 -0300

    index.jsx

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -5,13 +5,15 @@ function Menu(){
     return(
         <div className="Hope">
             <a className="logo" href="">Informatica NA</a>
-            <ul className="items">
-                <li><a href="#">INICIO</a></li>
-                <li><a href="#">CONTEÚDOS</a></li>
-                <li><a href="#">FORÚM</a></li>
-                <li><a href="#">SOBRE</a></li>
-                <li><a className="login" href="#">LOGIN</a></li>
-            </ul>
+            <nav>
+                <ul className="items">
+                    <li><a href="#">INICIO</a></li>
+                    <li><a href="#">CONTEÚDOS</a></li>
+                    <li><a href="#">FORÚM</a></li>
+                    <li><a href="#">SOBRE</a></li>
+                    <li><a className="login" href="#">LOGIN</a></li>
+                </ul>
+            </nav>
         </div>
     );
 }
