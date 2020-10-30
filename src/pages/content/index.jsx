cd863cfd7195083e98965433d3453e92cfd1358f
gustavo <gustavoantonio.pp@outlook.com>
2020-10-30 15:30:49 -0300

    rodapé atualizado e página de conteudos feita

--- src/pages/content/index.jsx
+++ src/pages/content/index.jsx
@@ -1,22 +1,60 @@
 import React from 'react';
+import arrowl from '../../assets/images/arrowl.png';
+import arrowr from '../../assets/images/arrowr.png';
+import wordlogo from '../../assets/images/word.png';
+import sqrexcel from '../../assets/images/sqrexcel.png';
+import sqrpp from '../../assets/images/sqrpp.png';
+import sqrgd from '../../assets/images/sqrgd.png';
+import sqrword from '../../assets/images/sqrword.png';
+
+import './index.css'
 
 function content(){
     return(
         <section>
-            <section className="slide">
+            <div className="slide">
                 <div className="slideshow">
                     <div className="arrowl">
-                        <img></img>
+                        <img src={arrowl}></img>
+                    </div>
+                    <div className="text">
+                        <div className="wordlogo">
+                            <img src={wordlogo}></img>
+                            <p>Word</p>
+                        </div>
+                        <div className="subtext">
+                            <p>Vídeo Intermediário II -</p>
+                            <p>Edição de cabeçalho e rodápe, alteração no estilo do título.</p>
+                            <p className="date">12/09/20</p>
+                        </div>
+                    </div>
+                    <div className="arrowr">
+                        <img src={arrowr}></img>
                     </div>
                 </div>
                 <div className="points">
-                    <div id="pt1"></div>
-                    <div id="pt2"></div>
-                    <div id="pt3"></div>
-                    <div id="pt4"></div>
+                    <div className="pt1"></div>
+                    <div className="pt2"></div>
+                    <div className="pt3"></div>
+                    <div className="pt4"></div>
                 </div>
-            </section>
-            <div className="content"></div>
+            </div>
+            <div className="content">
+                <div className="squares">
+                    <div className="sqr1">
+                        <a><img src={sqrword}></img></a>
+                    </div>
+                    <div className="sqr2">
+                        <a><img src={sqrexcel}></img></a>
+                    </div>
+                    <div className="sqr3">
+                        <a><img src={sqrgd}></img></a>
+                    </div>
+                    <div className="sqr4">
+                        <a><img src={sqrpp}></img></a>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
