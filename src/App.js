cc0d74fc34be9cab2d89ee9fa2a79cc774489c9d
João Vitor <vitorgrandinabreu@gmail.com>
2020-11-03 20:03:46 -0400

    att page sobre

--- src/App.js
+++ src/App.js
@@ -1,21 +1,5 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
-
-import Menu from './components/Menu'
-import Rodape from './components/Rodape';
-import Slider from './components/Slider';
-import Home from './pages/Home';
-import Sobre from './pages/Sobre';
-
-function App() {
-  return (
-    <div>
-      <Sobre />
-    </div>
-  );
-}
-=======
 import Home from './pages/Home';
 
 function App() {
@@ -23,5 +7,4 @@ function App() {
       <Home />
   )
 };
->>>>>>> 9adaa59e8eee00b926d587f9de9972611d74a360
 export default App;
