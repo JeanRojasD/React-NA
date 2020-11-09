aea4140e4e8d531a5535b2aa788e87e44635efa0
João Vitor <vitorgrandinabreu@gmail.com>
2020-11-08 20:26:38 -0400

    Att Sobree

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/Home';
+import Sobre from './pages/Sobre'
 
 function App() {
   return (
-      <Home />
+      <Sobre />
   )
 };
 export default App;
--- src/pages/Sobre/index.jsx
+++ src/pages/Sobre/index.jsx
@@ -13,18 +13,19 @@ function Sobre(){
 return(
   <div>
         <Menu />
-      <div className="textos">
+      <div className="textos1">
       <h1>Sobre Nós</h1>
       <p>Somos Estudantes Da Faculdade Uniamérica no Paraná, estamos cursando a faculdade de Engenharia De Software, </p>
       <p>Criamos essa plataforma através do nosso projeto Integrador e estamos trabalhando para levar-lo além, </p>                
       <p>acreditamos que muitas pessoas ainda faltam em conhecimento nessa área em específico, oque pode  </p>   
       <p>impactar muito em sua vida social.</p>
+      </div>
+      <div className="textos2">
       <h1>Quem Somos ?</h1>
       <p>Ensinar Cursos Essênciais para Iniciantes ou Inexperientes em informática basica, o aprendiz estará junto com  </p>    
       <p>agente absorvendo tudo que é ensinado gratuitamente em nossa plataforma de estudos que foi criada para ser </p>
       <p>100% online e gratuita facilitando assim o acesso á todos. </p>
       </div>
-      <div className='space'>              </div>
               <section className="fotos">
                 <img id="jean" src={Jean} alt="Jean Rojas"/>
                 <img id="gustavo" src={Gustavo} alt="Gustavo Antônio"/>
