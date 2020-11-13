bc423533fb1f8134ddd2d394cf86ad017a013378
gustavo <gustavoantonio.pp@outlook.com>
2020-11-13 00:43:16 -0300

    fixed slide and content

--- src/pages/content/index.jsx
+++ src/pages/content/index.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import './index.css'
-
 import Menu from '../../components/Menu';
 import Rodape from '../../components/Rodape';
-import Flecha from '../../components/flecha'
-import arrowl from '../../assets/images/arrowl.png';
-import arrowr from '../../assets/images/arrowr.png';
 import wordlogo from '../../assets/images/word.png';
 import exlogo from '../../assets/images/excel.png';
 import pplogo from '../../assets/images/powerpoint.png';
 import gdlogo from '../../assets/images/googledrive.png';
 
-import Slideshow from '../../components/slider';
+import Slideshow from '../../components/Slider-Content';
 
 function content() {
     return (
