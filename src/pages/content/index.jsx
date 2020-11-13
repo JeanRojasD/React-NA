4d60c33225a809b76d03e7b7fd317c804cb2bd16
gustavo <gustavoantonio.pp@outlook.com>
2020-11-13 14:06:27 -0300

    fixed tag problem on content page

--- src/pages/content/index.jsx
+++ src/pages/content/index.jsx
@@ -15,64 +15,63 @@ function Content() {
             <div className="contentbody">
                 <Slideshow />
             </div>
-                <div className="squares">
-                    <a href="/" className="sqr1">
-                        <div className="title">
-                            <img src={wordlogo} alt="Word"></img>
-                            <h1>Word</h1>
-                        </div>
-                        <div>
-                            <ul>
-                                <li>Vídeo</li>
-                                <li>Apostila</li>
-                                <li>Podcast</li>
-                                <li>+</li>
-                            </ul>
-                        </div>
-                    </a>
-                    <a href="/" className="sqr2">
-                        <div className="title">
-                            <img src={exlogo} alt="Excel"></img>
-                            <h1>Excel</h1>
-                        </div>
-                        <div>
-                            <ul>
-                                <li>Vídeo</li>
-                                <li>Apostila</li>
-                                <li>Podcast</li>
-                                <li>+</li>
-                            </ul>
-                        </div>
-                    </a>
-                    <a href="/" className="sqr3">
-                        <div className="title2">
-                            <img src={gdlogo} alt="Google Drive"></img>
-                            <h1>Google Drive</h1>
-                        </div>
-                        <div>
-                            <ul>
-                                <li>Vídeo</li>
-                                <li>Apostila</li>
-                                <li>Podcast</li>
-                                <li>+</li>
-                            </ul>
-                        </div>
-                    </a>
-                    <a href="/" className="sqr4">
-                        <div className="title2">
-                            <img src={pplogo} alt="Power Point"></img>
-                            <h1>Power Point</h1>
-                        </div>
-                        <div>
-                            <ul>
-                                <li>Vídeo</li>
-                                <li>Apostila</li>
-                                <li>Podcast</li>
-                                <li>+</li>
-                            </ul>
-                        </div>
-                    </a>
-                </div>
+            <div className="squares">
+                <a href="/" className="sqr1">
+                    <div className="title">
+                        <img src={wordlogo} alt="Word"></img>
+                        <h1>Word</h1>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>Vídeo</li>
+                            <li>Apostila</li>
+                            <li>Podcast</li>
+                            <li>+</li>
+                        </ul>
+                    </div>
+                </a>
+                <a href="/" className="sqr2">
+                    <div className="title">
+                        <img src={exlogo} alt="Excel"></img>
+                        <h1>Excel</h1>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>Vídeo</li>
+                            <li>Apostila</li>
+                            <li>Podcast</li>
+                            <li>+</li>
+                        </ul>
+                    </div>
+                </a>
+                <a href="/" className="sqr3">
+                    <div className="title2">
+                        <img src={gdlogo} alt="Google Drive"></img>
+                        <h1>Google Drive</h1>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>Vídeo</li>
+                            <li>Apostila</li>
+                            <li>Podcast</li>
+                            <li>+</li>
+                        </ul>
+                    </div>
+                </a>
+                <a href="/" className="sqr4">
+                    <div className="title2">
+                        <img src={pplogo} alt="Power Point"></img>
+                        <h1>Power Point</h1>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>Vídeo</li>
+                            <li>Apostila</li>
+                            <li>Podcast</li>
+                            <li>+</li>
+                        </ul>
+                    </div>
+                </a>
             </div>
             <Rodape />
         </div>
