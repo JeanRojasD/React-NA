dbf5c8a16d295dad721fb039ad633128d5332667
Eduardo Azevedo <69330807+TheRealLactose@users.noreply.github.com>
2020-11-14 12:54:45 -0300

    td finalizado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
-import Forumpag2 from './pages/Forum-page2';
+import Forum from './pages/Forum';
 function App() {
   return (
-    <Forumpag2/>
+    <Forum/>
   )
 };
 export default App;
