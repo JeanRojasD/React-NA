695c73a98bbdb03ef5b662c107d5a0d4e8c6fcdd
Eduardo Azevedo <69330807+TheRealLactose@users.noreply.github.com>
2020-12-03 19:13:34 -0300

    submit no forum

--- src/components/Forum-new-comment/index.jsx
+++ src/components/Forum-new-comment/index.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import Vector from '../../assets/images/vector.png'
 import './index.css'
+import { useState } from 'react'
 
 function Forum_new(props) {
+    const [duvida,setDuvida]=useState("");
+    const [title,setTitle]=useState("");
+    function commentarSubmit(event){
+        event.preventDefault();
+        console.log(title);
+        console.log(duvida);
+    }
+    const commentTitle=(e)=>{
+        setTitle(e.target.value);
+    }
+    const commentDuvida=(e)=>{
+        setDuvida(e.target.value);
+    }
     return (
         <div className="new-all">
             <div className="new-principal">
@@ -11,11 +25,12 @@ function Forum_new(props) {
                 </div>
             </div>
             <div className="new-all2">
+            <form onSubmit={commentarSubmit}>              
                 <div className="new-title">
-                    <input className="new-text" type="text" placeholder="Título"/>
+                    <input className="new-text" type="text" placeholder="Título" value={title} onChange={commentTitle}/>
                 </div>
                 <div className="new-duvida">
-                <input className="new-text2" type="text" placeholder="Escreva sobre sua dúvida"/>
+                <input className="new-text2" type="text" placeholder="Escreva sobre sua dúvida" value={duvida} onChange={commentDuvida}/> 
                 </div>
                 <div className="new-buttons">
                     <div className="new-cat">
@@ -31,10 +46,11 @@ function Forum_new(props) {
                             <button>Cancelar</button>
                         </div>
                         <div className="new-comentar">
-                            <button>Comentar</button>
+                            <button type="submit" value="comentar">Comentar</button>
                         </div>
                     </div>
                 </div>
+                </form>
             </div>
         </div>
     )
