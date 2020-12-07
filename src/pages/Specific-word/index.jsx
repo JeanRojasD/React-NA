466557aca6270533e59636dbee2cf59301ec0155
gustavo <gustavoantonio.pp@outlook.com>
2020-12-07 19:19:58 -0300

    fixed errors

--- src/pages/Specific-word/index.jsx
+++ src/pages/Specific-word/index.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import Header from '../../components/Header';
 import Rodape from '../../components/Rodape';
 import Summary from '../../components/Summary';
-import Sqr_specifics from '../../components/Sqr_specifics';
+import Sqrspecifics from '../../components/Sqr-specifics';
 import wordlogo from '../../assets/images/word.png';
 
 import './index.css'
 
-function Specific_word(){
+function Specificword(){
     const divStyle = {
         background: '#2A5893',
     };
@@ -17,9 +17,9 @@ function Specific_word(){
             <Summary style={divStyle} src={wordlogo} text="O Microsoft Word é software um editor de textos, cartas e documentos
                     desenvolvido pela Microsoft Office, foi criado por Richard Brodie para computadores
                     em 1983. Faz parte do conjunto de aplicativos Microsoft Office."/>
-            <Sqr_specifics style={divStyle}></Sqr_specifics>
+            <Sqrspecifics style={divStyle}></Sqrspecifics>
             <Rodape />
         </div>
     );
 }
-export default Specific_word;
+export default Specificword;
