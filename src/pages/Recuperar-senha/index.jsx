3e0766c236550213c629b85598cbc6cd56cd861c
João Vitor <vitorgrandinabreu@gmail.com>
2020-12-16 18:28:59 -0400

    Pagina de recuperação pronta

--- src/pages/Recuperar-senha/index.jsx
+++ src/pages/Recuperar-senha/index.jsx
@@ -2,42 +2,46 @@ import React from 'react';
 import Menu from '../../components/Menu'
 import Rodape from '../../components/Rodape'
 import Perfil from '../../assets/images/perfil.jpg'
+import Button from '@material-ui/core/Button';
 import './index.css'
 
-function Profile(){
-    return ( 
-    <div>
-        <Menu />
-        <section>
-            <div  className="geral">
-              <div className="blocos">
-              <div className="block1">
-                <img src={Perfil} alt="perfil img" width='250px' height='200px'/>
-                <button>Alterar</button>
-              </div>
-              <div className="block2">
-                  <label>
-                      <input type='text' name='Usuário' placeholder='Usuário'/>
-                      <button>Alterar</button>
-                  </label>
-                  <label>
-                      <input type='email' name='Email' placeholder='Email'/>
-                      <button>Alterar</button>
-                  </label>
-                  <label>
-                      <input type='password' name='Senha' placeholder='Senha'/>
-                      <button>Alterar</button>
-                  </label>
-                  <label className="btnend">
-                      <button>Cancelar</button>
-                      <button>Salvar</button>
-                  </label>
-              </div>
-            </div>
-          </div>
-        </section>
-        <Rodape />
-    </div>
+function Profile() {
+    return (
+        <div>
+            <Menu />
+            <section>
+                <div className="recuperar-geral">
+                    <div className="blocos-recuperar">
+                        <div className="block1-recuperar">
+                            <img src={Perfil} alt="perfil img" width='250px' height='200px' />
+                            <Button>Alterar</Button>
+                        </div>
+                        <div className="block2-recuperar">
+                            <a>ALTERE OS DADOS DA SUA CONTA</a>
+                            <label>
+                                <input type='text' name='Usuário' placeholder='Usuário' />
+                                <Button variant="contained">Alterar</Button>
+                            </label>
+                            <label>
+                                <input type='email' name='Email' placeholder='Email' />
+                                <Button variant="contained">Alterar</Button>
+                            </label>
+                            <label>
+                                <input type='password' name='Senha' placeholder='Senha' />
+                                <Button variant="contained">Alterar</Button>
+                            </label>
+                            <div>
+                                <div className="btnend-recuperar">
+                                    <Button variant="contained" color="primary">Salvar </Button>
+                                    <Button variant="contained" color="primary">Cancelar</Button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <Rodape />
+        </div>
     )
 }
 
